3cced849f6a1ac6cc6c8e19282d513e7e263d267
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-25 21:40:40 +0530

    window size and colour added

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from tkinter import *
 
 window=Tk()
-
-button=Button(window,text="OK",width=5,height=5,bg="black",fg="white")
-label=Label(window,text="Welcome")
+window.geometry("500x500")
+window.title("Simple Calculator")
+window.configure(bg="#676361")
+button=Button(window,text="OK",width=3,height=2,bg="black",fg="white")
+label=Label(window,text="submit")
 
 
 button.pack()
 label.pack()
+
 window.mainloop()
