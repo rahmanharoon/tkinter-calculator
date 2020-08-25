285cf9bd07b7ebfdb479ab8c35a2db08fb145418
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-25 21:27:06 +0530

    ok button added

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ from tkinter import *
 
 window=Tk()
 
+button=Button(window,text="OK",width=5,height=5,bg="black",fg="white")
+label=Label(window,text="Welcome")
+
+
+button.pack()
+label.pack()
 window.mainloop()
