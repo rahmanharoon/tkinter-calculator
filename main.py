177bb7f4d9be6575f73bfcb8d737c86323bce9ed
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-26 23:17:11 +0530

    equal to button can perform division

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def button_equal():
         e.insert(0, f_num * int(second_number))
 
     if math == "division":
-        e.insert(0, f_num + int(second_number))
+        e.insert(0, f_num / int(second_number))
 
 def button_substract():
     first_number = e.get()
