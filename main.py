350be6a7f6398ab9c1358724d5899ec707fbbdcf
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-26 23:21:05 +0530

    equal to button size resized

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ button8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(
 button9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
 button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
-button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
+button_equal = Button(root, text="=", padx=88, pady=20, command=button_equal)
 button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
 button_multiply = Button(root, text="X", padx=40, pady=20, command=button_multiply)
 button_divide = Button(root, text="/", padx=40, pady=20, command=button_divide)
