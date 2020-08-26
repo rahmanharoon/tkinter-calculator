af365f611b8a8a1f56d2726a89bd3d4283539dd3
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-26 22:09:01 +0530

    clear button fixed

--- main.py
+++ main.py
@@ -10,8 +10,15 @@ e=Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
 def button_click(number):
-    #e.delete(0, END)
-    e.insert(0, number)
+
+    current = e.get()
+    e.delete(0, END)
+    e.insert(0, str(current) + str(number))
+
+
+
+def button_clear():
+    e.delete(0, END)
 #button defining
 
 button1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
@@ -23,10 +30,10 @@ button6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(
 button7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
 button8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
 button9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
-button0 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(0))
+button0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
 buttonadd = Button(root, text="+", padx=39, pady=20, command=lambda: button_click())
 buttonequal = Button(root, text="=", padx=91, pady=20, command=lambda: button_click())
-buttonclear = Button(root, text="Clear", padx=79, pady=20, command=lambda: button_click())
+button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
 
 #button putting in screen
 button1.grid(row=3,column=0)
@@ -42,7 +49,7 @@ button8.grid(row=1,column=1)
 button9.grid(row=1,column=2)
 
 button0.grid(row=4, column=0)
-buttonclear.grid(row=4, column=1, columnspan=2)
+button_clear.grid(row=4, column=1, columnspan=2)
 buttonadd.grid(row=5, column=0)
 buttonequal.grid(row=5, column=1, columnspan=2)
 
